5b39f50c761ba864d784ae856f75b0563f2112cd
Eduardo <eduardo.guarienti@gmail.com>
2023-03-01 09:07:49 -0300

    Adicionando o ex 1 do modulo 2

---
+++ modulo_2/Modulo_02_ex_01.py
@@ -0,0 +1,8 @@
+unidade_sigla = 'UR'
+unidade_codigo = '0'
+unidade_classe_consumidora = 'A4-Verde-Comercial'
+unidade_conexao = 'Trifásica'
+unidade_condutor_tensao1 = '127'
+unidade_condutor_tensao2 = '220'
+
+conc = print('{}-{}-{}-{}-{}/{}' .format(unidade_sigla, unidade_codigo, unidade_classe_consumidora, unidade_conexao, unidade_condutor_tensao1, unidade_condutor_tensao2))
